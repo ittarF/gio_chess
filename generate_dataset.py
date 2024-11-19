0213216c905d4e8d4e18d7f44e9c0c594801198b
Giorgio Fratti <ittarf@gmail.com>
2024-11-19 15:15:31 +0100

    generate dataset from cli

--- generate_dataset.py
+++ generate_dataset.py
@@ -3,15 +3,26 @@
 # to run: ./generate_dataset.py
 
 import chess.pgn
-#import numpy as np
+import fire
+import numpy as np
 from board import Board
 
 
-def get_dataset(num_samples=None, path="data/lichess_db_standard_rated_2013-01.pgn"):
+def get_dataset(path, num_samples=None, output_file="dataset"):
+    """
+    Parse a PGN file and return a dataset of board states and results.
+    :param path: str, path to the PGN file
+    :param num_samples: int, number of samples to extract from the PGN file
+    :param output_file: str, name of the output file
+    :return: tuple, (X, y) where X is a list of board states and y is a list of results
+    
+    Example:
+    ./generate_dataset.py --path data/{file_name}.pgn --n 1000000 -o dataset_1M
+    """
     X, y = [], []
     values = {"1/2-1/2": 0, "0-1": -1, "1-0": 1}
     gn = 0
-    
+
     with open(path, "r", encoding="utf-8") as pgn_file:
         while True:
             game = chess.pgn.read_game(pgn_file)
@@ -22,15 +33,19 @@ def get_dataset(num_samples=None, path="data/lichess_db_standard_rated_2013-01.p
                 board.board.push(move)
                 X.append(board.encode())
                 y.append(values[game.headers["Result"]])
-            gn += 1    
-            print(f"Game {gn:8} parsed | total samples: {len(X)}", end="\r")        
+            gn += 1
+            print(f"Game {gn:8} parsed | total samples: {len(X)}", end="\r")
             if num_samples is not None and len(X) >= num_samples:
                 break
 
-    return X, y
+    X, y = np.array(X), np.array(y)
+
+    np.savez(f"processed/{output_file}.npz", X, y)
+    
+    print(f"\nDataset saved to processed/{output_file}.npz")
+    return
 
 
 if __name__ == "__main__":
 
-    X_a, y_a = get_dataset()
-    print(len(X_a), len(y_a))
+    fire.Fire(get_dataset)
