180cd5bb9f9a2e5364404a0e93f9fd47f8a411ca
Giorgio Fratti <ittarf@gmail.com>
2024-11-24 17:20:31 +0100

    minimax?

--- engine.py
+++ engine.py
@@ -4,27 +4,30 @@ import torch
 
 class Engine:
     def __init__(self):
-        from train import Net
+        from models import ChessEvaluatorTransformer as Net
 
-        params = torch.load("models/model1310.pth")
+        params = torch.load("models/t_1301.pth")
         self.model = Net()
         self.model.load_state_dict(params)
         self.model.eval()
 
     def find_best(self, brd):
+        """
+        Given a board state return best move along with its evaluation
+        """
         import numpy as np
 
-        legal_moves = brd.board.legal_moves
+        legal_moves = brd.legal_moves
         inp = []
         for move in legal_moves:
-            brd.board.push(move)
+            brd.push(move)
             inp.append(brd.encode())
-            brd.board.pop()
+            brd.pop()
         with torch.no_grad():
             out = self.model(torch.tensor(np.array(inp)).float())
 
         b_move = None
-        if brd.board.turn:
+        if brd.turn:
             t, b_val = 1, -100
         else:
             t, b_val = -1, 100
@@ -33,12 +36,32 @@ class Engine:
             if t * val > t * b_val:
                 b_val = val
                 b_move = mv
-        return b_move
+        return b_move, b_val
+
+    def minimax(self, brd, depth=1):
+
+        # depth = 1
+
+        b_val = 100
+        b_move = None
+
+        for move in brd.legal_moves:
+            brd.push(move)
+            _, val_oppo = self.find_best(brd)  # find best move of oppo (max)
+            brd.pop()
+            if val_oppo < b_val:
+                b_move = move
+                b_val = val_oppo
+
+        return b_move, b_val
 
 
 if __name__ == "__main__":
     from board import Board
+    import time
 
     engine = Engine()
     board = Board()
-    print(engine.find_best(board))
+    s = time.time()
+    print(engine.minimax(board))
+    print(time.time() - s)
